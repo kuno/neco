5adbbdf90f077d8a57cf55a1b4269fcba36d24cb
kuno <neokuno@gmail.com>
2010-12-29 15:02:16 +0800

    ts

--- shell/install_node.sh
+++ shell/install_node.sh
@@ -29,32 +29,34 @@ if [ -e /usr/bin/python2 ] || [ -e /usr/local/bin/python2 ]; then
       sed -i 's_^#!.*/usr/bin/python_#!/usr/bin/python2_' $file
       sed -i 's_^#!.*/usr/bin/env.*python_#!/usr/bin/env python2_' $file
     done
+  fi
 
-    if [ -e tools/node-waf ]; then
-      for file in $(find . -name '*.py' -print) tools/node-waf; do
-        sed -i 's_^#!.*/usr/bin/python_#!/usr/bin/python2_' $file
-        sed -i 's_^#!.*/usr/bin/env.*python_#!/usr/bin/env python2_' $file
-      done
+  if [ -e tools/node-waf ]; then
+    for file in $(find . -name '*.py' -print) tools/node-waf; do
+      sed -i 's_^#!.*/usr/bin/python_#!/usr/bin/python2_' $file
+      sed -i 's_^#!.*/usr/bin/env.*python_#!/usr/bin/env python2_' $file
+    done
+  fi
 
-      for file in $(find . -name '*.py' -print) wscript; do
-        sed -i 's_^#!.*/usr/bin/python_#!/usr/bin/python2_' $file
-        sed -i 's_^#!.*/usr/bin/env.*python_#!/usr/bin/env python2_'     $file
-      done
+  for file in $(find . -name '*.py' -print) wscript; do
+    sed -i 's_^#!.*/usr/bin/python_#!/usr/bin/python2_' $file
+    sed -i 's_^#!.*/usr/bin/env.*python_#!/usr/bin/env python2_'     $file
+  done
 
-      sed -i "s|cmd_R = 'python |cmd_R = 'python2 |" wscript
+  sed -i "s|cmd_R = 'python |cmd_R = 'python2 |" wscript
 
-      ./configure --prefix=/ecosystem/ || return 1
-      sed -i "s|python |python2 |" Makefile  
-    else
-      ./configure --prefix=/ecosystem/ || return 1 
-    fi
+  ./configure --prefix=/ecosystem/ || return 1
+  sed -i "s|python |python2 |" Makefile  
+else
+  ./configure --prefix=/ecosystem/ || return 1 
+fi
 
-    make || return 1
+make || return 1
 
-    mkdir -p $distdir/ecosystem/{bin,etc,lib,include,share,man}
+mkdir -p $distdir/ecosystem/{bin,etc,lib,include,share,man}
 
-    tools/waf-light install --destdir=$distdir
+tools/waf-light install --destdir=$distdir
 
-    install -D -m644 LICENSE $distdir/ecosystem/share/licenses/node/LICENSE
-    install -D -m644 ChangeLog $distdir/ecosystem/share/node/ChangeLog
-    install -D -m644 README $distdir/ecosystem/share/node/README
+install -D -m644 LICENSE $distdir/ecosystem/share/licenses/node/LICENSE
+install -D -m644 ChangeLog $distdir/ecosystem/share/node/ChangeLog
+install -D -m644 README $distdir/ecosystem/share/node/README
