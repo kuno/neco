477e5eef7611d984326558ae2b01d350f38c1f8f
kuno <neokuno@gmail.com>
2010-12-29 15:23:52 +0800

    nts

--- shell/install_node.sh
+++ shell/install_node.sh
@@ -40,13 +40,13 @@ if [ -e /usr/bin/python2 ] || [ -e /usr/local/bin/python2 ]; then
   fi
 
   if [ -d tools/scons ]; then
-    for file in $(find tools/scons -name '*.py' -print); do
+    for file in $(find tools/scons/ -name '*.py' -print); do
       sed -i 's_^#!.*/usr/bin/python_#!/usr/bin/python2_' $file
       sed -i 's_^#!.*/usr/bin/env.*python_#!/usr/bin/env python2_' $file
-    done
+    done                                                                                                     gg
   fi
 
-  for file in $(find . -name '*.py' -print) wscript; do
+  for file in $(find . -name '*.py' -print) wscript tools/waf tools/waf-light tools/node-waf ; do
     sed -i 's_^#!.*/usr/bin/python_#!/usr/bin/python2_' $file
     sed -i 's_^#!.*/usr/bin/env.*python_#!/usr/bin/env python2_'     $file
   done
@@ -63,7 +63,7 @@ make || return 1
 
 mkdir -p $distdir/ecosystem/{bin,etc,lib,include,share,man}
 
-if [ -e tools/waf-light ];then
+if [ -e tools/waf-light ]; then
   tools/waf-light install --destdir=$distdir
 elif [ -e tools/waf ]; then
   tools/waf install --destdir=$distdir
