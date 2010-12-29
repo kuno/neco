b05a22d8aeea08dfc4f33064bbce5c6faa91a043
kuno <neokuno@gmail.com>
2010-12-29 13:43:45 +0800

    nts

--- cmd/help.js
+++ cmd/help.js
@@ -7,14 +7,17 @@ exports.run = function(cmd) {
     console.log('neco - Nodejs Ecosystem COordinator, like virtualenv for python.');
     console.log('Usage:\n  nc <command>');
     console.log('Currently where <command> should be on of the below commands:\n');
-    console.log('  create    -  create new node ecosystem');
+    console.log('  create      -  create new node ecosystem');
     console.log('     Useage: nc create <id> [node-version]');
     console.log('');
-    console.log('  list      -  list all installed node ecosystem');
+    console.log('  list        -  list all installed node ecosystem');
     console.log('     Usage: nc list');
     console.log('');
-    console.log('  activate  -  how to activate a existing node ecosystem.');
+    console.log('  activate    -  show how to activate a existing node ecosystem.');
     console.log('     Usage: nc activate <id>');
+    console.log('');
+    console.log('  deactivate  -  show how to deactivate a active ecosystem.');
+    console.log('     Usage: nc deactivate [id]');
     console.log('');
     console.log('Git repository: http://github.com/kuno/neco');
   } else {
