5179fce7f7c9c8a62aa9d8b2128ea41c7a41688c
kuno <neokuno@gmail.com>
2010-12-29 10:06:42 +0800

    nts

--- cmd/list.js
+++ cmd/list.js
@@ -1,10 +1,9 @@
 var fs = require('fs'),
 path = require('path'),
+display = require('../lib/console.js').display,
 recordFile = path.join(process.env.NECO_ROOT, '.neco/record.json');
 
 exports.run = function() {
   ecosystems = JSON.parse(fs.readFileSync(recordFile, 'utf8')).ecosystems;
-  ecosystems.forEach(function(e) {
-    console.log('ID: '+e.id+' Node Ver: '+e.nv+' Created: '+e.cd+' NPM: '+e.npm);
-  });
+  display(ecosystems);
 };
