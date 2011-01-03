e62f12a2cbaae3dc05acae0ef4be8a8da4762bd5
kuno <neokuno@gmail.com>
2011-01-03 18:42:54 +0800

    nts

--- cmd/activate.js
+++ cmd/activate.js
@@ -1,8 +1,9 @@
 var path = require('path'),
-color = require('ansi-color').set;
+color = require('ansi-color').set,
+pkgDir = path.join(__dirname, '..');
 
 exports.run = function(config) {
-  var act = path.join(config.pkgDir, 'shell/activate.sh');
+  var act = path.join(pkgDir, 'shell/activate.sh');
   var shell = color('source '+act, 'bold+yellow');
   console.log();
   console.log('add \''+shell+'\' to your .bashrc file, :)');
