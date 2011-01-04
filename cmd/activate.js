0df7340dc13cb4c3966d37911c09d130ed5b1668
kuno <neokuno@gmail.com>
2011-01-04 17:22:11 +0800

    nts

--- cmd/activate.js
+++ cmd/activate.js
@@ -4,7 +4,6 @@ color = require('ansi-color').set;
 exports.run = function(config) {
   var act = path.join(config.pkgDir, 'shell/activate.sh');
   var shell = color('source '+act, 'bold+yellow');
-  console.log();
   console.log('add \''+shell+'\' to your .bashrc or .zshrc file, :)');
   console.log('Then run '+color('neco_activate '+config.id, 'bold+yellow')+' in you shell.');
 };
