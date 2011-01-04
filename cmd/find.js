3e7e7c2f5ae2408e836ccedc6f4848ce50d851b4
kuno <neokuno@gmail.com>
2011-01-04 18:35:33 +0800

    nts

--- cmd/find.js
+++ cmd/find.js
@@ -3,12 +3,12 @@ path = require('path'),
 show = require('../lib/console.js').showReleases;
 
 exports.run = function(config) {
-  var version, releases = [], release;
+  var releases = [], release;
   fs.readFile(config.distFile, 'utf8', function(err, data) {
     if (err) {throw err;}
     if (config.target) {
       release = getRelease(config);
-      releases.push(release);
+      releases[0] = release;
     } else {
       console.log(data);
       releases = JSON.parse(data).history;
