15dd2a11c42a85069932d9f8a6f99f303537e8fb
kuno <neokuno@gmail.com>
2011-01-04 19:07:02 +0800

    nts

--- cmd/help.js
+++ cmd/help.js
@@ -9,19 +9,22 @@ exports.run = function(config) {
     console.log('');
     console.log('Currently where <command> should be on of the below commands:\n');
     console.log('  create      -  create new node ecosystem');
-    console.log('     Useage: neco create <id> [node-version]');
+    console.log('      Useage: neco create <id> [node-version]');
     console.log('');
     console.log('  list        -  list all installed node ecosystem');
-    console.log('     Usage: neco list');
+    console.log('      Usage:  neco list');
+    console.log('');
+    console.log('  find        - find out available node release(s)');
+    console.log('      Usage:  neco find [stable, latest, <version>]');
     console.log('');
     console.log('  activate    -  show how to activate an existing node ecosystem');
-    console.log('     Usage: neco activate <id>');
+    console.log('      Usage:  neco activate <id>');
     console.log('');
     console.log('  deactivate  -  show how to deactivate an active node ecosystem');
-    console.log('     Usage: neco deactivate [id]');
+    console.log('      Usage:  neco deactivate [id]');
     console.log('');
     console.log('  help        -  show help information');
-    console.log('     Usage: neco help');
+    console.log('      Usage:  neco help');
     console.log('');
     console.log('Git repository: http://github.com/kuno/neco');
   } else {
