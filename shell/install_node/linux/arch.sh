31ba47747fe19a69b1b7eeb2b741bde062bc02a0
kuno <neokuno@gmail.com>
2011-01-04 13:33:16 +0800

    removed old node dir

--- shell/install_node/linux/arch.sh
+++ shell/install_node/linux/arch.sh
@@ -8,9 +8,9 @@ else
   cd $targetDir/../source
 fi
 
-#if [ -d node-$ver ]; then
-#  rm -rf node-$ver
-#fi
+if [ -d node-$ver ]; then
+  rm -rf node-$ver
+fi
 
 if [ ! -e node-$ver.tar.gz ]; then
   #curl -O $link || return 1
