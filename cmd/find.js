252a9d2997f517db9c0bd6313b94e731957a3783
kuno <neokuno@gmail.com>
2011-01-05 11:00:12 +0800

    nts

--- cmd/find.js
+++ cmd/find.js
@@ -4,15 +4,16 @@ show = require('../lib/display.js').showReleases,
 getRelease = require('../lib/assistant.js').getRelease;
 
 exports.run = function(config) {
-  var releases = [], release;
+  var release;
+  config.releases = [];
   fs.readFile(config.distFile, 'utf8', function(err, data) {
     if (err) {throw err;}
     if (config.target) {
       release = getRelease(config);
-      releases[0] = release;
+      config.releases[0] = release;
     } else {
-      releases = JSON.parse(data).history;
+      config.releases = JSON.parse(data).history;
     }
-    show(releases);
+    show(config);
   });
 };
--- cmd/list.js
+++ cmd/list.js
@@ -3,6 +3,6 @@ path = require('path'),
 show = require('../lib/display.js').showEcosystems;
 
 exports.run = function(config) {
-  var ecosystems = JSON.parse(fs.readFileSync(config.recordFile, 'utf8')).ecosystems;
-  show(ecosystems);
+  config.ecosystems = JSON.parse(fs.readFileSync(config.recordFile, 'utf8')).ecosystems;
+  show(config.ecosystems);
 };
