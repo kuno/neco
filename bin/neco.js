a1ef0e2399db42d36208c2c369f1a22ffca45b54
kuno <neokuno@gmail.com>
2011-01-06 10:38:37 +0800

    added config.globalActivateFile

--- bin/neco.js
+++ bin/neco.js
@@ -18,7 +18,8 @@ getRelease = require('../lib/assistant.js').getRelease;
 
 var getConfiguration = require('../lib/config.js').getConfiguration, 
 envReady = require('../lib/inception.js').envReady,
-recordReady = require('../lib/inception.js').recordReady;
+recordReady = require('../lib/inception.js').recordReady,
+activateReady = require('../lib/inception.js').activateReady;
 
 var configuration = getConfiguration(), id, cmd = process.argv[2];
 var message, warning, error, suggestion, example;
@@ -32,36 +33,38 @@ if (isCMDValid(cmd) === false) {
   // Subcommand create
   if (cmd === 'create') {
     envReady(configuration, function(config) {
-      recordReady(config, function(exists, config) {
-        if (process.argv.length < 4) {
-          message = 'Missing ID';
-          suggestion = 'Please specific at least one ID( and the version of node, if you will).';
-          example = 'neco create <id> [stable, latest, node-version]';
-          log('message', message, suggestion, example);
-        } else {
-          config.id = process.argv[3];
-          config.cmd = cmd;
-          config.target = process.argv[4] || 'stable';
-
-          if (isIDValid(config) === false) {
-            message = 'The given id '+config.id+' is one of the reserved words in neco.';
-            suggestion = 'Please choose another one.';
+      activateReady(config, function(config) {
+        recordReady(config, function(exists, config) {
+          if (process.argv.length < 4) {
+            message = 'Missing ID';
+            suggestion = 'Please specific at least one ID( and the version of node, if you will).';
+            example = 'neco create <id> [stable, latest, node-version]';
             log('message', message, suggestion, example);
-
           } else {
-            if (!exists) {
-              create.run(config);
-            } else {  
-              if (isIDUnique(config) === false) {
-                message = 'The given id '+config.id+' has already been used.';
-                suggestion = 'Please choose another one instead.';
-                log('message', message, suggestion);
-              } else {
+            config.id = process.argv[3];
+            config.cmd = cmd;
+            config.target = process.argv[4] || 'stable';
+
+            if (isIDValid(config) === false) {
+              message = 'The given id '+config.id+' is one of the reserved words in neco.';
+              suggestion = 'Please choose another one.';
+              log('message', message, suggestion, example);
+
+            } else {
+              if (!exists) {
                 create.run(config);
+              } else {  
+                if (isIDUnique(config) === false) {
+                  message = 'The given id '+config.id+' has already been used.';
+                  suggestion = 'Please choose another one instead.';
+                  log('message', message, suggestion);
+                } else {
+                  create.run(config);
+                }
               }
             }
           }
-        }
+        });
       });
     });
   }
@@ -69,11 +72,13 @@ if (isCMDValid(cmd) === false) {
   // Subcommand list
   else if (cmd === 'list') {
     envReady(configuration, function(config) {
-      recordReady(config, function(exists, config) {
-        if (exists) {
-          config.cmd = cmd;
-          list.run(config);
-        }
+      activateReady(config, function(config) {
+        recordReady(config, function(exists, config) {
+          if (exists) {
+            config.cmd = cmd;
+            list.run(config);
+          }
+        });
       });
     });
   }
@@ -81,21 +86,23 @@ if (isCMDValid(cmd) === false) {
   // Subcommand find
   else if (cmd === 'find') {
     envReady(configuration, function(config) {
-      recordReady(config, function(exists, config) {
-        config.cmd = cmd;
-        if (process.argv.length >= 4) {
-          config.target = process.argv[3];
-          if (getRelease(config)) {
-            find.run(config);
+      activateReady(config, function(config) {
+        recordReady(config, function(exists, config) {
+          config.cmd = cmd;
+          if (process.argv.length >= 4) {
+            config.target = process.argv[3];
+            if (getRelease(config)) {
+              find.run(config);
+            } else {
+              error = 'The desired release '+config.target+' is not available.'
+              suggestion = 'Find out all the aviable releases.';
+              example = 'neco find [stable, latest, node-version]';
+              log('error', error, suggestion, example);
+            }
           } else {
-            error = 'The desired release '+config.target+' is not available.'
-            suggestion = 'Find out all the aviable releases.';
-            example = 'neco find [stable, latest, node-version]';
-            log('error', error, suggestion, example);
+            find.run(config);
           }
-        } else {
-          find.run(config);
-        }
+        });
       });
     });
   }
@@ -108,32 +115,34 @@ if (isCMDValid(cmd) === false) {
   // Subcommand activate
   else if (cmd === 'activate') {
     envReady(configuration, function(config) {
-      recordReady(config, function(exists, config) {
-        if (exists) {
-          if (process.argv.length < 4) {
-            message = 'Missing ID';
-            suggestion = 'Please specify the id of the ecosystem you want to activate.';
-            example = 'neco activate <id>';
-            log('message', message, suggestion, example);
-          } else {
-            id = process.argv[3];
-            config.id = id;
-            config.cmd = cmd;
-
-            if (isActive(config) === true) {
-              warning = 'The node ecosystem with id '+id+' is already active.';
-              suggstion = 'Please use type deact in your shell to deactivate it.';
-              log('warning', warning, suggestion, example);
-            } else if (isIDExsit(config) == false) {
-              warning = 'The node ecosystem with id '+id+' is not exists.';
-              suggestion = 'You can use neco list command to find out all existing ecosystem.';
-              example = 'neco create <id> [node-version]';
-              log('warning', warning, suggestion, example);
+      activateReady(config, function(config) {
+        recordReady(config, function(exists, config) {
+          if (exists) {
+            if (process.argv.length < 4) {
+              message = 'Missing ID';
+              suggestion = 'Please specify the id of the ecosystem you want to activate.';
+              example = 'neco activate <id>';
+              log('message', message, suggestion, example);
             } else {
-              activate.run(config);
+              id = process.argv[3];
+              config.id = id;
+              config.cmd = cmd;
+
+              if (isActive(config) === true) {
+                warning = 'The node ecosystem with id '+id+' is already active.';
+                suggstion = 'Please use type deact in your shell to deactivate it.';
+                log('warning', warning, suggestion, example);
+              } else if (isIDExsit(config) == false) {
+                warning = 'The node ecosystem with id '+id+' is not exists.';
+                suggestion = 'You can use neco list command to find out all existing ecosystem.';
+                example = 'neco create <id> [node-version]';
+                log('warning', warning, suggestion, example);
+              } else {
+                activate.run(config);
+              }
             }
           }
-        }
+        });
       });
     });
   }
@@ -141,38 +150,42 @@ if (isCMDValid(cmd) === false) {
   // Subcommand deactvate
   else if (cmd === 'deactivate') {
     envReady(configuration, function(config) {
-      recordReady(config, function(exists, config) {
-        if (exists) {
-          if (process.argv.length >= 4) {
-            id = process.argv[3];
-            config.id = id;
-            config.cmd = cmd;
-            if (isActive(config) === false) {
-              warning = 'The node ecosystem with id '+id+' is not active.';
-              suggestion = 'Use neco activate command to activate one first.';
-              example = 'neco activate <id>';
-              log('warning', warning, suggestion, example);
-            } else if (isIDExsit(config) == false) {
-              warning = 'The node ecosystem with id '+id+' is not exists.';
-              suggestion = 'You can use neco list command to find out all existing ecosystem.';
-              example = 'neco list';
-              log('warning', warning, suggestion, example);
-            } else {
-              deactivate.run(config);
+      activateReady(config, function(config) {
+        recordReady(config, function(exists, config) {
+          if (exists) {
+            if (process.argv.length >= 4) {
+              id = process.argv[3];
+              config.id = id;
+              config.cmd = cmd;
+              if (isActive(config) === false) {
+                warning = 'The node ecosystem with id '+id+' is not active.';
+                suggestion = 'Use neco activate command to activate one first.';
+                example = 'neco activate <id>';
+                log('warning', warning, suggestion, example);
+              } else if (isIDExsit(config) == false) {
+                warning = 'The node ecosystem with id '+id+' is not exists.';
+                suggestion = 'You can use neco list command to find out all existing ecosystem.';
+                example = 'neco list';
+                log('warning', warning, suggestion, example);
+              } else {
+                deactivate.run(config);
+              }
             }
+            deactivate.run(config);
           }
-          deactivate.run(config);
-        }
+        });
       });
     });
   }
 
   // Subcommand destory
-  else if (cmd === 'destroy') {
+  else if (cmd === 'remove') {
     envReady(configuration, function(config) {
-      recordReady(config, function(exists, config) {
-        if (exists) {
-        }
+      activateReady(config, function(config) {
+        recordReady(config, function(exists, config) {
+          if (exists) {
+          }
+        });
       });
     });
   }
--- cmd/activate.js
+++ cmd/activate.js
@@ -2,8 +2,7 @@ var path = require('path'),
 color = require('ansi-color').set;
 
 exports.run = function(config) {
-  var act = path.join(config.pkgDir, 'shell/activate.sh');
-  var shell = color('source '+act, 'bold+yellow');
+  var shell = color('source '+config.globalActivateFile, 'bold+yellow');
   console.log('add \''+shell+'\' to your .bashrc or .zshrc file, :)');
   console.log('Then run '+color('neco_activate '+config.id, 'bold+yellow')+' in you shell.');
 };
