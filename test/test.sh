0a8174fc9315b3e719301d2e3de0f12f0e6ecf69
kuno <neokuno@gmail.com>
2011-01-14 14:49:41 +0800

    added test/testDist.json

--- test/test.sh
+++ test/test.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env sh
 
-unset $NECO_ROOT
+unset NECO_ROOT
 
-neco
+../bin/neco.js
 
 export NECO_ROOT=/tmp
 
-neco create test0 stable
+if [ -e ../data/dist.json.bak ]; then
+  mv ../data/dist.json.bak ../data/dist.json || return 1
+else 
+  cp ../data/dist.json ../data/dist.json.bak || return 1
+  cp ./testDist.json ../data/dist.json || return 1
+fi
+node ../bin/neco.js create test0 stable
 
-neco create test1 latest
+node ../bin/neco.js create test1 latest
 
-neco create test2 
+node ../bin/neco.js create test2 
 
-neco create test3 0.1.100
+node ../bin/neco.js create test3 0.1.100
 
-neco remove test0
+node ../bin/neco.js remove test0
 
-neco remove test1
+node ../bin/neco.js remove test1
 
-neco remove test2
+node ../bin/neco.js remove test2
 
+mv ../data/dist.json.bak ../data/dist.json || return 1
 exit 0
 
