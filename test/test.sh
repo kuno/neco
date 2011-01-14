0dba11696c25613463b2a6305139e98363ce4189
kuno <neokuno@gmail.com>
2011-01-14 15:25:46 +0800

    nts

--- test/test.sh
+++ test/test.sh
@@ -1,31 +1,37 @@
 #!/usr/bin/env sh
-
 unset NECO_ROOT
-
-../bin/neco.js
-
 export NECO_ROOT=/tmp
+export PATH=$PWD/../bin:$PATH
+
+neco.js
 
 if [ -e ../data/dist.json.bak ]; then
-  mv ../data/dist.json.bak ../data/dist.json || return 1
+  mv ../data/dist.json.bak ../data/dist.json
 else 
-  cp ../data/dist.json ../data/dist.json.bak || return 1
-  cp ./testDist.json ../data/dist.json || return 1
+  cp ../data/dist.json ../data/dist.json.bak
+  cp ./testDist.json ../data/dist.json
 fi
-node ../bin/neco.js create test0 stable
 
-node ../bin/neco.js create test1 latest
+neco.js create test0 stable
 
-node ../bin/neco.js create test2 
+neco.js create test1 latest
 
-node ../bin/neco.js create test3 0.1.100
+neco.js create test2 
 
-node ../bin/neco.js remove test0
+neco.js create test3 0.1.100
 
-node ../bin/neco.js remove test1
+neco.js remove test0
 
-node ../bin/neco.js remove test2
+neco.js remove test1
 
-mv ../data/dist.json.bak ../data/dist.json || return 1
+neco.js remove test2
+
+neco js remove test3
+
+if [ -e ../data/dist.json.bak ] && [ -e ../data/dist.json ]; then
+  mv ../data/dist.json.bak ../data/dist.json
+fi
+rm -rf NECO_ROOT/.neco || return 1
+
 exit 0
 
