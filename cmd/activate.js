3fb3b681ffd0b4e35562c5805cbb5e118af60ebf
kuno <neokuno@gmail.com>
2011-01-15 10:45:30 +0800

    tell user how to deactivate when they ask how to activate

--- cmd/activate.js
+++ cmd/activate.js
@@ -3,6 +3,8 @@ color = require('ansi-color').set;
 
 exports.run = function(config) {
   var shell = color('source '+config.globalActivateFile, 'bold+yellow');
+  var deactivate = color('neco_deactivate', 'bold+yellow');
   console.log('add \''+shell+'\' to your .bashrc or .zshrc file, :)');
   console.log('Then run '+color('neco_activate '+config.id, 'bold+yellow')+' in you shell.');
+  console.log('To deactivate it, run \''+deactivate+'\' in you shell.');
 };
