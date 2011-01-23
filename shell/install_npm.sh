8a8b1ae9bfec702ddd35d4bd359b0c0e715b789c
kuno <neokuno@gmail.com>
2011-01-23 14:24:14 +0800

    sed before npm installation

--- shell/install_npm.sh
+++ shell/install_npm.sh
@@ -14,16 +14,16 @@ if [ ! -e $destDir/bin/node ]; then
   return 1
 fi
 
-# Make global and user config
+# Replace with root, binroot, manroot with destDir
+sed -i -e "s/^root.*/root\ =\ $replace\/ecosystem\/lib\/node/g" $pkgDir/sample/npmrc || return 1
+sed -i -e "s/^binroot.*/binroot\ =\ $replace\/ecosystem\/bin/g" $pkgDir/sample/npmrc || return 1
+sed -i -e "s/^manroot.*/manroot\ = \ $replace\/ecosystem\/share\/man/g" $pkgDir/sample/npmrc || return 1
+
+# Set npm global and user config
 export npm_config_globalconfig=$destDir/ecosystem/etc/npmrc || return 1
 export npm_config_userconfig=$destDir/npmrc || return 1
 install -m666 $pkgDir/sample/npmrc $destDir/ecosystem/etc/npmrc || return 1 
 install -m666 $pkgDir/sample/npmrc $destDir/npmrc || return 1
-
-# Replace with root, binroot, manroot with destDir
-sed -i -e "s/^root.*/root\ =\ $replace\/ecosystem\/lib\/node/g" $pkgDir/sample/npmrc || return 1
-sed -i -e "s/^binroot.*/binroot\ =\ $replace\/ecosystem\/bin/g" $pkgDir/sample/npmrc || return 1
-sed -i -e "s/^manroot.*/manroot\ = \ $replace\/ecosystem\/share\/man/g" $pkgDir/sample/npmrc || return 1  
 
 node $pkgDir/deps/npm/cli.js config set root $destDir/ecosystem/lib/node --flags &> /dev/null || return 1
 node $pkgDir/deps/npm/cli.js config set binroot $destDir/ecosystem/bin --flags || &> /dev/null return 1
